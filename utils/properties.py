bfd723d632ea6322558b361670d16eb17716792b
Jan Cas <jan.l.cas@outlook.com>
2026-02-05 21:02:02 -0500

    utils

---
+++ utils/properties.py
@@ -0,0 +1,89 @@
+from equinox import Module, field
+import jax
+from jaxtyping import Float, Array
+import jax.numpy as jnp
+from pathlib import Path
+from typing import Union
+import numpy as np
+from CoolProp.CoolProp import PropsSI
+
+def psat_water(T: float) -> float:
+    return PropsSI("P", "T", T, "Q", 0, "Water")
+
+def rh_to_c(rh, T):
+    return rh*psat_water(T)/(IDEAL_GAS_CONST*T)
+
+IDEAL_GAS_CONST = 8.314 #J/molK
+WATER_MOLAR_MASS = .018 # kg/mol
+
+class EnvironmentalConditions(Module):
+    
+    @staticmethod
+    def _c_to_k_converter(T: float) -> float:
+        if T < 200: return T+273
+
+        return T
+    
+    T: Float = field(converter=_c_to_k_converter)
+    RH: Float
+
+    @property
+    def C_amb(self):
+        return rh_to_c(self.RH, self.T)
+
+
+class BedProperties(Module):
+    sorbent_bed_height: Float
+    sorbent_bed_width: Float
+    sorbent_bed_length: Float
+
+    pore_diameter: Float
+    porosity: Float
+
+    vapor_diffusivity: Float
+
+    @property
+    def knudsen_diffusivity(self):
+        return (self.pore_diameter / 3) * \
+                jnp.sqrt((8*IDEAL_GAS_CONST*(20+273))/(jnp.pi * WATER_MOLAR_MASS))
+
+    @property
+    def bed_diffusivity(self):
+        return self.porosity**(3/2) * ((1/self.vapor_diffusivity + 1/self.knudsen_diffusivity) ** (-1))
+
+class Isotherm(Module):
+    C: Array
+    n_eq: Array
+
+    
+    @staticmethod
+    def g_per_g_to_mol_per_kg(n_eq):
+        return n_eq / WATER_MOLAR_MASS
+
+    @classmethod
+    def read_from_file(cls, path: Union['str', Path], env: EnvironmentalConditions) -> Isotherm:
+        path = Path(path)
+
+        data = np.loadtxt(path)
+
+        C = rh_to_c(data[:, 0], env.T)
+        n_eq = cls.g_per_g_to_mol_per_kg(data[:, 1])
+
+        return cls(C=C, n_eq=n_eq)
+    
+    def __call__(self, concentration):
+        return jnp.interp(concentration, self.C, self.n_eq)
+
+class SorbentProperties(Module):
+    particle_radius: Float
+    particle_diffusivity: Float
+    particle_density: Float
+    isotherm: Module
+
+    @property
+    def k_sorb(self) -> Float:
+        return 15 * self.particle_diffusivity / (self.particle_radius**2)
+
+    def __call__(self, concentration):
+        return self.isotherm(concentration)
+
---
+++ utils/spatial_discretization.py
@@ -0,0 +1,47 @@
+import equinox as eqx
+from jaxtyping import Array, Float
+from typing import Callable
+import jax
+import jax.numpy as jnp
+
+# Represents the interval [x0, x_final] discretised into n equally-spaced points.
+class SpatialDiscretisation(eqx.Module):
+    x0: float = eqx.field(static=True)
+    x_final: float = eqx.field(static=True)
+    vals: Float[Array, "n"]
+
+    @classmethod
+    def discretise_fn(cls, x0: float, x_final: float, n: int, fn: Callable):
+        if n < 2:
+            raise ValueError("Must discretise [x0, x_final] into at least two points")
+        vals = jax.vmap(fn)(jnp.linspace(x0, x_final, n))
+        return cls(x0, x_final, vals)
+
+    @property
+    def δx(self):
+        return (self.x_final - self.x0) / (len(self.vals) - 1)
+
+    def binop(self, other, fn):
+        if isinstance(other, SpatialDiscretisation):
+            if self.x0 != other.x0 or self.x_final != other.x_final:
+                raise ValueError("Mismatched spatial discretisations")
+            other = other.vals
+        return SpatialDiscretisation(self.x0, self.x_final, fn(self.vals, other))
+
+    def __add__(self, other):
+        return self.binop(other, lambda x, y: x + y)
+
+    def __mul__(self, other):
+        return self.binop(other, lambda x, y: x * y)
+
+    def __radd__(self, other):
+        return self.binop(other, lambda x, y: y + x)
+
+    def __rmul__(self, other):
+        return self.binop(other, lambda x, y: y * x)
+
+    def __sub__(self, other):
+        return self.binop(other, lambda x, y: x - y)
+
+    def __rsub__(self, other):
+        return self.binop(other, lambda x, y: y - x)
